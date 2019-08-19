99c6a33994006fe248ed222a036cc5e111c79e4e
chris-poe <chris.poe92@gmail.com>
2019-08-19 12:20:05 -0400

    Add Jest-Enzyme and configure adapter

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('App - renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders', () => {
+    shallow(<App />);
+  });
 });
